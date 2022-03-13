04d2de7008a5de81c756bb3782cab834287be84f
Orhan <ya.eskimo-callboy@yandex.by>
2022-03-13 23:31:07 +0300

    upload project to heroku

--- routes/add-new-friend.js
+++ routes/add-new-friend.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const userService = require("../service/user-service");
 /* Post request for /add-new-friend starts */
 router.post("/add-new-friend", async (req, res) => {
-  const friendEmail = req.body.email; //email пользователя которого хочу добавить
-  const currentUserId = req.body.id; //мой id
-
   try {
+    const friendEmail = req.body.email; //email пользователя которого хочу добавить
+    const currentUserId = req.body.id; //мой id
+    
     await userService.addFriend(friendEmail, currentUserId);
     res.status(200).send({ success: "Friend has been added" });
   } catch (error) {
     console.log(error);
-    res.status(error.statusCode).send({error: "Friend cannot execute request"});
+    res
+      .status(error.statusCode)
+      .send({ error: "Friend cannot execute request" });
   }
 });
 /* Post request for /add-new-friend ends */
