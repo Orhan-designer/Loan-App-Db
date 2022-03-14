080d0aea2cfe5942977c9a5fa584aa3e1bc34fa9
Orhan <ya.eskimo-callboy@yandex.by>
2022-03-14 14:40:38 +0300

    try

--- routes/add-new-friend.js
+++ routes/add-new-friend.js
@@ -6,14 +6,14 @@ router.post("/add-new-friend", async (req, res) => {
   try {
     const friendEmail = req.body.email; //email пользователя которого хочу добавить
     const currentUserId = req.body.id; //мой id
-    
+
     await userService.addFriend(friendEmail, currentUserId);
     res.status(200).send({ success: "Friend has been added" });
   } catch (error) {
     console.log(error);
     res
       .status(error.statusCode)
-      .send({ error: "Friend cannot execute request" });
+      .send({ error: "You cannot be a friend with yourself" });
   }
 });
 /* Post request for /add-new-friend ends */
